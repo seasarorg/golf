8aa9364c3d0b5e2b9670786fdaf13f277ccfb699
Masanobu Shimura <mikeshimura@gmail.com>
2007-01-29 11:38:52 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/golf/trunk@57 330ffdcb-1ddb-4dd4-8add-47e3d2c5bf7b

--- golf/src/org/seasar/golf/uexample/ldb/allcommon/cbean/SelectResource.java
+++
@@ -1,9 +0,0 @@
-package org.seasar.golf.uexample.ldb.allcommon.cbean;
-
-/**
- * The select-resource as marker-interface.
- * 
- * @author DBFlute(AutoGenerator)
- */
-public interface SelectResource {
-}
