4931c6b72f601dea5e30550b8c67e6ec2afe5042
Masanobu Shimura <mikeshimura@gmail.com>
2007-02-10 22:46:09 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/golf/trunk@88 330ffdcb-1ddb-4dd4-8add-47e3d2c5bf7b

---
+++ GolfRMI/src/org/seasar/golf/transaction/impl/GolfTrxServiceImpl.java
@@ -0,0 +1,23 @@
+package org.seasar.golf.transaction.impl;
+
+import org.seasar.framework.container.factory.SingletonS2ContainerFactory;
+import org.seasar.golf.data.RequestData;
+import org.seasar.golf.data.ResultData;
+import org.seasar.golf.transaction.GolfTrxService;
+import org.seasar.golf.transaction.TransactionInterface;
+
+public class GolfTrxServiceImpl implements GolfTrxService {
+
+	public ResultData execute(RequestData requestData) {
+        //S2Container cont = SingletonS2ContainerFactory.getContainer();
+        TransactionInterface transaction = 
+                (TransactionInterface) SingletonS2ContainerFactory.getContainer().
+                getComponent(requestData.getTransactionCode() + "TrxLogic");
+        ResultData res = transaction.execute(requestData);
+         return res;		
+//		ResultData resultData = new ResultData();
+//		resultData.getFormAction().setFormStack(FormAction.FormStack.RESULT);
+//		return resultData;
+	}
+
+}
