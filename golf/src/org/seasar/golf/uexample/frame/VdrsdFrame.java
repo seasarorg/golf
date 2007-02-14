3014f57620264a02e3c14bc6aa2d74ac26944f2a
Masanobu Shimura <mikeshimura@gmail.com>
2007-02-14 21:41:13 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/golf/trunk@99 330ffdcb-1ddb-4dd4-8add-47e3d2c5bf7b

--- golf/src/org/seasar/golf/uexample/frame/VdrsdFrame.java
+++ golf/src/org/seasar/golf/uexample/frame/VdrsdFrame.java
@@ -9,21 +9,24 @@ package org.seasar.golf.uexample.frame;
 import java.util.HashMap;
 import javax.swing.JMenuBar;
 import javax.swing.JPanel;
+import javax.swing.ListSelectionModel;
 import org.seasar.golf.GolfFormInterface;
 import org.seasar.golf.GolfTableModel;
 import org.seasar.golf.data.RequestData;
 import org.seasar.golf.form.FormManager;
+import org.seasar.golf.util.TableUtil;
 import org.seasar.golf.transaction.RequestDataFactory;
+import org.seasar.golf.transaction.TrxUtil;
 
 /**
  *
  * @author  shimura
  */
-public class VdrsFrame extends javax.swing.JFrame implements GolfFormInterface{
+public class VdrsdFrame extends javax.swing.JFrame implements GolfFormInterface{
     private FormManager formManager = null;
     private GolfTableModel golfTableModel= new GolfTableModel();    
     /** Creates new form VdrFrame */
-    public VdrsFrame() {
+    public VdrsdFrame() {
         initComponents();
     }
     
@@ -43,13 +46,8 @@ public class VdrsFrame extends javax.swing.JFrame implements GolfFormInterface{
         jB_Back = new javax.swing.JButton();
         jB_Menu = new javax.swing.JButton();
         jB_NewWindow = new javax.swing.JButton();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jTextCode = new javax.swing.JTextField();
-        jTextShort = new javax.swing.JTextField();
-        jTextName = new javax.swing.JTextField();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
         jScrollPane1 = new javax.swing.JScrollPane();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
@@ -111,58 +109,51 @@ public class VdrsFrame extends javax.swing.JFrame implements GolfFormInterface{
         });
         toolBar.add(jB_NewWindow);
 
-        jLabel1.setText("\u30b3\u30fc\u30c9");
-
-        jLabel2.setText("\u7701\u7565\u540d");
-
-        jLabel3.setText("\u540d\u79f0");
-
-        jLabel4.setText("\u4e0b\u8a18\u9805\u76ee\u3067\u9078\u629e\u3067\u304d\u307e\u3059\uff08\u524d\u65b9\u4e00\u81f4\uff09");
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jTable1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jTable1MouseClicked(evt);
+            }
+        });
+        jTable1.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                jTable1KeyPressed(evt);
+            }
+        });
+        jScrollPane2.setViewportView(jTable1);
 
         org.jdesktop.layout.GroupLayout contentPaneLayout = new org.jdesktop.layout.GroupLayout(contentPane);
         contentPane.setLayout(contentPaneLayout);
         contentPaneLayout.setHorizontalGroup(
             contentPaneLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-            .add(toolBar, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 871, Short.MAX_VALUE)
+            .add(contentPaneLayout.createSequentialGroup()
+                .add(303, 303, 303)
+                .add(jScrollPane1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 424, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
+            .add(toolBar, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 727, Short.MAX_VALUE)
             .add(contentPaneLayout.createSequentialGroup()
                 .addContainerGap()
-                .add(contentPaneLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                    .add(contentPaneLayout.createSequentialGroup()
-                        .add(contentPaneLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                            .add(jLabel1)
-                            .add(jLabel3)
-                            .add(jLabel2))
-                        .add(15, 15, 15)
-                        .add(contentPaneLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING, false)
-                            .add(jTextName, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 94, Short.MAX_VALUE)
-                            .add(jTextShort)
-                            .add(jTextCode)))
-                    .add(jLabel4))
-                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED, 254, Short.MAX_VALUE)
-                .add(jScrollPane1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 425, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
+                .add(jScrollPane2, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 703, Short.MAX_VALUE)
+                .addContainerGap())
         );
         contentPaneLayout.setVerticalGroup(
             contentPaneLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
             .add(contentPaneLayout.createSequentialGroup()
                 .add(toolBar, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                .add(contentPaneLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                    .add(contentPaneLayout.createSequentialGroup()
-                        .add(jLabel4)
-                        .add(16, 16, 16)
-                        .add(contentPaneLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
-                            .add(jLabel1)
-                            .add(jTextCode, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
-                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                        .add(contentPaneLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
-                            .add(jLabel2)
-                            .add(jTextShort, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
-                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                        .add(contentPaneLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
-                            .add(jLabel3)
-                            .add(jTextName, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)))
-                    .add(jScrollPane1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 100, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
-                .add(343, 343, 343))
+                .add(jScrollPane1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 100, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                .add(jScrollPane2, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 317, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                .add(20, 20, 20))
         );
 
         jMenu1.setText("Menu");
@@ -174,15 +165,13 @@ public class VdrsFrame extends javax.swing.JFrame implements GolfFormInterface{
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-            .add(org.jdesktop.layout.GroupLayout.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .add(layout.createSequentialGroup()
                 .add(contentPane, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
+                .addContainerGap(org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
             .add(layout.createSequentialGroup()
-                .addContainerGap()
                 .add(contentPane, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 482, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
@@ -191,9 +180,18 @@ public class VdrsFrame extends javax.swing.JFrame implements GolfFormInterface{
     }// </editor-fold>//GEN-END:initComponents
 
 private void jB_EnterActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jB_EnterActionPerformed
-    RequestData requestData = RequestDataFactory.createRequestData("vdrs","mode=test",formManager);
+    RequestData requestData = RequestDataFactory.createRequestData("vdr","mode=test",formManager);
+    TrxUtil.setAllTableDataToRequest(requestData, formManager);
     formManager.getSession().trxExecute(requestData, formManager);
 }//GEN-LAST:event_jB_EnterActionPerformed
+
+private void jTable1KeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jTable1KeyPressed
+
+}//GEN-LAST:event_jTable1KeyPressed
+
+private void jTable1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTable1MouseClicked
+
+}//GEN-LAST:event_jTable1MouseClicked
 
 private void jB_NewWindowActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jB_NewWindowActionPerformed
     formManager.getSession().processMenuAction("@MENU");
@@ -217,7 +215,7 @@ private void jTextActionKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:ev
             }
         });
     }
-  
+    
     public FormManager getFormManager() {
         return formManager;
     }
@@ -234,7 +232,11 @@ private void jTextActionKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:ev
         formManager = new FormManager(this); 
         formManager.init();
         formManager.createReportList(jScrollPane1);
-        formManager.setValidationFromCsvResource("vdrs_bind.csv");
+        jTable1.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        formManager.SetTableColumnFromCsvResource(
+    			jTable1, "VendorTable", "VendorTable", golfTableModel, "Vdrsd_table.csv");
+        TableUtil.SetPreferedColumnWIdth(jTable1, new int[]{15,40,60,150,80,0 });  
+
     }
 
     public void processAction(HashMap params) {
@@ -247,17 +249,12 @@ private void jTextActionKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:ev
     private javax.swing.JButton jB_Menu;
     private javax.swing.JButton jB_NewWindow;
     private javax.swing.JButton jB_Save;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTable jTable1;
     private javax.swing.JTextField jTextAction;
-    private javax.swing.JTextField jTextCode;
-    private javax.swing.JTextField jTextName;
-    private javax.swing.JTextField jTextShort;
     private javax.swing.JPanel toolBar;
     // End of variables declaration//GEN-END:variables
     
