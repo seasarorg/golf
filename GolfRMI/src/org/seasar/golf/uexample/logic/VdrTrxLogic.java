b480cf74f99c49aa7cf657f35c7a93c26fbd3b6a
Masanobu Shimura <mikeshimura@gmail.com>
2007-03-04 12:36:22 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/golf/trunk@156 330ffdcb-1ddb-4dd4-8add-47e3d2c5bf7b

--- GolfRMI/src/org/seasar/golf/uexample/logic/VdrTrxLogic.java
+++ GolfRMI/src/org/seasar/golf/uexample/logic/VdrTrxLogic.java
@@ -9,10 +9,15 @@
 
 package org.seasar.golf.uexample.logic;
 
+import java.math.BigDecimal;
+
 import org.seasar.golf.data.RequestData;
 import org.seasar.golf.data.ResultData;
 import org.seasar.golf.form.FormAction;
+import org.seasar.golf.form.FormAction.FormStack;
 import org.seasar.golf.transaction.TransactionInterface;
+import org.seasar.golf.uexample.dao.exdao.CompanyDao;
+import org.seasar.golf.uexample.dao.exentity.Company;
 import org.seasar.golf.validation.Severity;
 import org.seasar.golf.validation.message.SimpleValidationMessage;
 import org.seasar.golf.validator.HostTableFieldInfo;
@@ -22,7 +27,7 @@ import org.seasar.golf.validator.HostTableFieldInfo;
  * @author shimura
  */
 public class VdrTrxLogic implements TransactionInterface{
-    
+    private CompanyDao companyDao;
     /** Creates a new instance of VdrTransaction */
     public VdrTrxLogic() {
     }
@@ -32,12 +37,38 @@ public class VdrTrxLogic implements TransactionInterface{
 	 */
     public ResultData execute(RequestData requestData) {
         ResultData resultData = new ResultData();
-        resultData.getFormAction().setFormStack(FormAction.FormStack.RESULT);
-        resultData.getValidationResult().add(new SimpleValidationMessage(
-        		"Text:ERROR DAYO", Severity.ERROR, "Text1"));
-        resultData.getValidationResult().add(new SimpleValidationMessage(
-        		"Table ERROR HOT DAYOoo", Severity.ERROR, new HostTableFieldInfo("TestTable",1,"qty")));        
+        processMode(requestData, resultData);
+//        resultData.getFormAction().setFormStack(FormAction.FormStack.RESULT);
+//        resultData.getValidationResult().add(new SimpleValidationMessage(
+//        		"Text:ERROR DAYO", Severity.ERROR, "Text1"));
+//        resultData.getValidationResult().add(new SimpleValidationMessage(
+//        		"Table ERROR HOT DAYOoo", Severity.ERROR, new HostTableFieldInfo("TestTable",1,"qty")));        
         return resultData;
     }
+    private void processMode(RequestData requestData, ResultData resultData){
+    	String mode = (String) requestData.getParams().get("mode");
+    	if (mode.equals("NextInq")){
+    		BigDecimal ccode = (BigDecimal) requestData.getParams().get("ccode");
+    		Company comp = companyDao.getEntity(ccode);
+    		if (comp == null){
+    	        resultData.getValidationResult().add(new SimpleValidationMessage(
+        		"CCode not found (" + ccode.toString()+")", Severity.ERROR));
+    		} else {
+    			resultData.getFormAction().setFormStack(FormStack.NEXT);
+    			resultData.getFormAction().setForm("vdr");
+    			resultData.getParams().put("mode", "ref");
+    			resultData.getFields().put("ccode",comp.getCcode());
+    			resultData.getFields().put("shortname",comp.getShortname());
+    			resultData.getFields().put("name",comp.getName());
+    			resultData.getFields().put("telephone",comp.getTelephone());
+    			resultData.getFields().put("cat",comp.getCat());
+    			resultData.getFields().put("version",comp.getVersionno().toString());
+    		}
+    	}
+    }
+
+	public void setCompanyDao(CompanyDao companyDao) {
+		this.companyDao = companyDao;
+	}
 
 }
