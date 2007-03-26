4217b9aaf93eb52f4199eb9f2276fc04a82c80d8
Masanobu Shimura <mikeshimura@gmail.com>
2007-03-26 14:24:57 +0000

    git-svn-id: https://www.seasar.org/svn/sandbox/golf/trunk@213 330ffdcb-1ddb-4dd4-8add-47e3d2c5bf7b

--- golf/src/org/seasar/golf/ColumnDef.java
+++ golf/src/org/seasar/golf/ColumnDef.java
@@ -34,10 +34,21 @@ public class ColumnDef {
     public ColumnDef() {
         
     }
+    /**
+     * 
+     * @param name 
+     */
     public ColumnDef(String name) {
         this.name = name;
         
     }
+    /**
+     * 
+     * @param name 
+     * @param type 
+     * @param canEdit 
+     * @param hostName 
+     */
     public ColumnDef(String name, Class type, boolean canEdit, String hostName ) {
 
         this.name = name;
@@ -45,6 +56,15 @@ public class ColumnDef {
         this.canEdit = canEdit;
         this.hostName = hostName;
     }
+     /**
+      * 
+      * @param name 
+      * @param type 
+      * @param canEdit 
+      * @param insertCheckColumn 
+      * @param validatorDef 
+      * @param hostName 
+      */
      public ColumnDef(String name, Class type, boolean canEdit,boolean  insertCheckColumn, 
              ValidatorDef validatorDef, String hostName ) {
         this.name = name;
@@ -56,50 +76,98 @@ public class ColumnDef {
         
     }
 
+    /**
+     * 
+     * @return 
+     */
     public ValidatorDef getValidatorDef() {
         return validatorDef;
     }
 
+    /**
+     * 
+     * @param validatorDef 
+     */
     public void setValidatorDef(ValidatorDef validatorDef) {
         this.validatorDef = validatorDef;
     }
 
+    /**
+     * 
+     * @return 
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * 
+     * @param name 
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * 
+     * @return 
+     */
     public boolean getCanEdit() {
         return canEdit;
     }
 
+    /**
+     * 
+     * @param canEdit 
+     */
     public void setCanEdit(boolean canEdit) {
         this.canEdit = canEdit;
     }
 
+    /**
+     * 
+     * @return 
+     */
     public Class getType() {
         return type;
     }
 
+    /**
+     * 
+     * @param type 
+     */
     public void setType(Class type) {
         this.type = type;
     }
 
+    /**
+     * 
+     * @return 
+     */
     public boolean getInsertCheckColumn() {
         return insertCheckColumn;
     }
 
+    /**
+     * 
+     * @param insertCheckColumn 
+     */
     public void setInsertCheckColumn(boolean insertCheckColumn) {
         this.insertCheckColumn = insertCheckColumn;
     }
 
+    /**
+     * 
+     * @return 
+     */
     public String getHostName() {
         return hostName;
     }
 
+    /**
+     * 
+     * @param hostName 
+     */
     public void setHostName(String hostName) {
         this.hostName = hostName;
     }
